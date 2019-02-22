f91455b76b3b73c614c924d592b58862f96af6d0
Dmitry Fonarev <dfonarev@e1z2r4p11.42.us.org>
2019-02-22 02:18:19 -0800

    edit

--- brainfuck.c
+++ brainfuck.c
@@ -3,20 +3,20 @@
 int	main(int argc, char **argv)
 {
 	char	string[2048];
-	char	*str = &string[0];
+	char	*str;
 	char	*ptr;
-	char	*tmp;
 	int		i;
 
 	if (argc != 2)
 		return (0);
 	i = 0;
-	while (i < 2048)
+	while (i < 200)
 	{
-		str[i] = '\0';
+		string[i] = 0;
 		i++;
 	}
 	ptr = *(argv + 1);
+	str = &string[0];
 	while (*ptr)
 	{
 		if (*ptr == '>')
@@ -26,13 +26,34 @@ int	main(int argc, char **argv)
 		else if (*ptr == '+')
 			(*str)++;
 		else if (*ptr == '-')
-			(*str)++;
+			(*str)--;
 		else if (*ptr == '.')
 			write(1, str, 1);
-		else if (*ptr == '[' && *str == 0)
-			tmp = ptr;
-		else if (*ptr == ']' && *str != 0)
-			ptr = tmp;
+		else if (*ptr == '[' && !*str)
+		{
+
+			i = 1;
+			while (i > 0)
+			{
+				ptr++;
+				if (*ptr == '[')
+					i++;
+				else if (*ptr == ']')
+					i--;
+			}
+		}
+		else if (*ptr == ']' && *str)
+		{
+			i = 1;
+			while (i > 0)
+			{
+				ptr--;
+				if (*ptr == ']')
+					i++;
+				else if (*ptr == '[')
+					i--;
+			}
+		}
 		ptr++;
 	}
 }
